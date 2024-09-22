554c7c1981ca6c0b401dd5c9d1ed0190e755d0e1
ArtAEurope <ingrid@askart.se>
2024-09-22 09:10:38 +0200

    "Lagt till färgrutor och text"

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,4 +1,4 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import { Image, StyleSheet, Platform, View, Text } from 'react-native';
 
 import { HelloWave } from '@/components/HelloWave';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
@@ -7,46 +7,73 @@ import { ThemedView } from '@/components/ThemedView';
 
 export default function HomeScreen() {
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({ ios: 'cmd + d', android: 'cmd + m' })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+    
+<View style={{backgroundColor: "#59c4f6", flex:1,}}>
+  <View style={{backgroundColor: "#e94d3c", height: 150, margin:2}}>
+    <View style={{flex:1, justifyContent:"center", alignItems:"center",}}>
+      <Text style={{ fontSize: 30}}>RÖD</Text>
+    </View>
+</View>  
+
+  <View style={{backgroundColor: "white", height:150, flexDirection:"row", margin: 2}}>
+      <View style= {{backgroundColor:"#63c465", width:"50%", flex:1, }}>
+        <View style= {{flex:1, justifyContent:"flex-start",}}>
+          <Text style={{fontSize:32}}>GRÖN</Text>
+      </View>
+      
+      </View>
+      
+      <View style= {{backgroundColor:"#f6cd46",width:"50%", flex:1}}></View>
+
+  </View>
+
+  <View style= {{backgroundColor: "white", height: 80, flexDirection: "row", margin:2}}>
+    <View style= {{backgroundColor: "#e94d3c", flex:2,}}></View>
+    <View style= {{backgroundColor: "black", flex:1 }}></View>
+     </View>
+  
+<View style= {{flex:1}}></View>
+
+<View style= {{backgroundColor: "#ee8c00", height: 50,margin:2}}>
+  <View style={{flex:1, justifyContent:"flex-end", alignItems:"flex-end",}}>
+    <Text style={{fontSize:32}}>ORANGE</Text>
+    </View>
+
+</View>
+
+
+
+
+
+
+
+</View>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    
+   
+   
+   
+
+  
   );
 }
 
